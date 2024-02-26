4d17060901655fafc92cf73d03e70f3a0f01ff87
CROWM51 <136242009+CROWM51@users.noreply.github.com>
2024-02-26 20:38:11 +0800

    Add files via upload

---
+++ MDK-ARM/GXURC_R2/startup_stm32f427xx.d
@@ -0,0 +1,1 @@
+gxurc_r2\startup_stm32f427xx.o: startup_stm32f427xx.s
---
+++ MDK-ARM/GXURC_R2/stream_buffer.d
@@ -0,0 +1,14 @@
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c
+gxurc_r2\stream_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+gxurc_r2\stream_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+gxurc_r2\stream_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+gxurc_r2\stream_buffer.o: ../Core/Inc/FreeRTOSConfig.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+gxurc_r2\stream_buffer.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/stream_buffer.h
